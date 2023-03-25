16c90f305b0804ba43eaba334bca661e89c9f292
KLA-pie <klieatjam@olin.edu>
2023-03-25 03:25:17 -0400

    Adjusted multiplication matricies to Kronecker products

--- Quantum-Functions/expected_value.py
+++ Quantum-Functions/expected_value.py
@@ -20,16 +20,12 @@ def statevector_output(gate_list):
         as indicies of all qubit combinations, the statevector's respective complex
         conjugate statevector, and the probability density vector of all qubit combinations.
     """
-    statevector_0 = np.array([1, 0])
-    statevector_1 = np.array([1, 0])
+    statevector = np.array([1, 0, 0, 0])
     conjugate_statevector = []
     qubit_probabilities = []
     for i, gate in enumerate(gate_list):
         gate_composition = gate_matrix(gate)
-        if "0)" in gate:
-            statevector = np.matmul(gate_composition, statevector_0)
-        else:
-            statevector = np.matmul(gate_composition, statevector_1)
+        statevector = np.matmul(gate_composition, statevector)
 
     for i in range(4):
         conjugate_value = np.conjugate(statevector[i])
@@ -57,189 +53,18 @@ def gate_matrix(gate):
         A 4 by 4 transformation matrix with respect to the gate and the qubit
         input to update the statevector after passed through a quantum gate.
     """
+    iden_matrix = np.array([[1, 0], [0, 1]])
     if "," in gate and ".cx(" not in gate and ".swap(" not in gate:
         float_int = gate.find("(") + 1
         float_end = gate.find(",")
         theta = float(gate[float_int:float_end])
 
-    if ".h(" in gate:
-        if "0" in gate:
-            return np.array([[2**-0.5, 2**-0.5], [1 * 2**-0.5, -1 * 2**-0.5]])
-        return np.array(
-            [
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, 2**-0.5, 2**-0.5],
-                [0, 0, 2**-0.5, -1 * 2**-0.5],
-            ]
-        )
-    elif ".x(" in gate:
-        if "0" in gate:
-            return np.array([[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        return np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])
-    elif ".y(" in gate:
-        if "0" in gate:
-            return np.array([[0, -1j, 0, 0], [1j, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        return np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, -1j], [0, 0, 1j, 0]])
-    elif ".z(" in gate:
-        if "0" in gate:
-            return np.array([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        return np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, -1]])
-    elif ".p(" in gate:
-        phase_output = np.cos(theta) + np.sin(theta) * 1j
-        if "0)" in gate:
-            return np.array(
-                [[1, 0, 0, 0], [0, phase_output, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
-            )
-        return np.array(
-            [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, phase_output]]
-        )
-    elif ".s(" in gate:
-        if "0" in gate:
-            return np.array([[1, 0, 0, 0], [0, 1j, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        return np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1j]])
-    elif ".sdg(" in gate:
-        if "0" in gate:
-            return np.array([[1, 0, 0, 0], [0, -1j, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-        return np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, -1j]])
-    elif ".t(" in gate:
-        if "0" in gate:
-            return np.array(
-                [
-                    [1, 0, 0, 0],
-                    [0, 2**-0.5 + (2**-0.5) * 1j, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1],
-                ]
-            )
-        return np.array(
-            [
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, 1, 0],
-                [0, 0, 0, 2**-0.5 + (2**-0.5) * 1j],
-            ]
-        )
-    elif ".tdg(" in gate:
-        if "0" in gate:
-            return np.array(
-                [
-                    [1, 0, 0, 0],
-                    [0, 2**-0.5 - (2**-0.5) * 1j, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1],
-                ]
-            )
-        return np.array(
-            [
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, 1, 0],
-                [0, 0, 0, 2**-0.5 - (2**-0.5) * 1j],
-            ]
-        )
+    if ".swap(" in gate:
+        return np.array([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
     elif ".cx(" in gate:
         if "(0" in gate:
             return np.array([[1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0], [0, 1, 0, 0]])
         return np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])
-    elif ".swap(" in gate:
-        return np.array([[1, 0, 0, 0], [0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
-    elif ".sx(" in gate:
-        if "0" in gate:
-            return np.array(
-                [
-                    [0.5 + 0.5j, 0.5 - 0.5j, 0, 0],
-                    [0.5 - 0.5j, 0.5 + 0.5j, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1],
-                ]
-            )
-        return np.array(
-            [
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, 0.5 + 0.5j, 0.5 - 0.5j],
-                [0, 0, 0.5 - 0.5j, 0.5 + 0.5j],
-            ]
-        )
-    elif ".sxdg(" in gate:
-        if "0" in gate:
-            return np.array(
-                [
-                    [0.5 - 0.5j, 0.5 + 0.5j, 0, 0],
-                    [0.5 + 0.5j, 0.5 - 0.5j, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1],
-                ]
-            )
-        return np.array(
-            [
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, 0.5 - 0.5j, 0.5 + 0.5j],
-                [0, 0, 0.5 + 0.5j, 0.5 - 0.5j],
-            ]
-        )
-    elif ".rx(" in gate:
-        real_comp = np.cos(theta / 2)
-        imag_comp = -1j * np.sin(theta / 2)
-        if "0)" in gate:
-            return np.array(
-                [
-                    [real_comp, imag_comp, 0, 0],
-                    [imag_comp, real_comp, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1],
-                ]
-            )
-        return np.array(
-            [
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, real_comp, imag_comp],
-                [0, 0, imag_comp, real_comp],
-            ]
-        )
-    elif ".ry(" in gate:
-        real_comp_cos = np.cos(theta / 2)
-        real_comp_sin = np.sin(theta / 2)
-        if "0)" in gate:
-            return np.array(
-                [
-                    [real_comp_cos, -1 * real_comp_sin, 0, 0],
-                    [real_comp_sin, real_comp_cos, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1],
-                ]
-            )
-        return np.array(
-            [
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, real_comp_cos, -1 * real_comp_sin],
-                [0, 0, real_comp_sin, real_comp_cos],
-            ]
-        )
-    elif ".rz(" in gate:
-        euler_identity = np.cos(theta / 2) + 1j * np.sin(theta / 2)
-        euler_identity_conj = np.cos(theta / 2) + -1j * np.sin(theta / 2)
-        if "0)" in gate:
-            return np.array(
-                [
-                    [euler_identity_conj, 0, 0, 0],
-                    [0, euler_identity, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1],
-                ]
-            )
-        return np.array(
-            [
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, euler_identity_conj, 0],
-                [0, 0, 0, euler_identity],
-            ]
-        )
     elif ".rxx(" in gate:
         real_comp = np.cos(theta / 2)
         imag_comp = -1j * np.sin(theta / 2)
@@ -273,6 +98,50 @@ def gate_matrix(gate):
                 [0, 0, 0, euler_identity_conj],
             ]
         )
+    elif ".h(" in gate:
+        transform_matrix = np.array(
+            [[2**-0.5, 2**-0.5], [2**-0.5, -1 * 2**-0.5]]
+        )
+    elif ".x(" in gate:
+        transform_matrix = np.array([[0, 1], [1, 0]])
+    elif ".y(" in gate:
+        transform_matrix = np.array([[0, -1j], [1j, 0]])
+        return np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, -1j], [0, 0, 1j, 0]])
+    elif ".z(" in gate:
+        transform_matrix = np.array([[1, 0], [0, -1]])
+    elif ".p(" in gate:
+        phase_output = np.cos(theta) + np.sin(theta) * 1j
+        transform_matrix = np.array([[1, 0], [0, phase_output]])
+    elif ".s(" in gate:
+        transform_matrix = np.array([[1, 0], [0, 1j]])
+    elif ".sdg(" in gate:
+        transform_matrix = np.array([[1, 0], [0, -1j]])
+    elif ".t(" in gate:
+        transform_matrix = np.array([[1, 0], [0, 2**-0.5 + (2**-0.5)]])
+    elif ".tdg(" in gate:
+        transform_matrix = np.array([[1, 0], [0, 2**-0.5 - (2**-0.5)]])
+    elif ".sx(" in gate:
+        transform_matrix = np.array(
+            [[0.5 + 0.5j, 0.5 - 0.5j], [0.5 - 0.5j, 0.5 + 0.5j]]
+        )
+    elif ".sxdg(" in gate:
+        transform_matrix = np.array(
+            [[0.5 - 0.5j, 0.5 + 0.5j], [0.5 + 0.5j, 0.5 - 0.5j]]
+        )
+    elif ".rx(" in gate:
+        real_comp = np.cos(theta / 2)
+        imag_comp = -1j * np.sin(theta / 2)
+        transform_matrix = np.array([[real_comp, imag_comp], [imag_comp, real_comp]])
+    elif ".ry(" in gate:
+        real_comp_cos = np.cos(theta / 2)
+        real_comp_sin = np.sin(theta / 2)
+        transform_matrix = np.array(
+            [[real_comp_cos, -1 * real_comp_sin], [real_comp_sin, real_comp_cos]]
+        )
+    elif ".rz(" in gate:
+        euler_identity = np.cos(theta / 2) + 1j * np.sin(theta / 2)
+        euler_identity_conj = np.cos(theta / 2) + -1j * np.sin(theta / 2)
+        transform_matrix = np.array([[euler_identity_conj, 0], [0, euler_identity]])
     elif ".u(" in gate:
         gate = gate[float_end + 2 : :]
         float_end = gate.find(",")
@@ -284,26 +153,14 @@ def gate_matrix(gate):
         entry_2 = -1 * (np.cos(lam) + 1j * np.sin(lam)) * np.sin(theta / 2)
         entry_3 = (np.cos(phi) + 1j * np.sin(phi)) * np.sin(theta / 2)
         entry_4 = (np.cos(phi + lam) + 1j * np.sin(phi + lam)) * np.cos(theta / 2)
-        if "0)" in gate:
-            return np.array(
-                [
-                    [entry_1, entry_2, 0, 0],
-                    [entry_3, entry_4, 0, 0],
-                    [0, 0, 1, 0],
-                    [0, 0, 0, 1],
-                ]
-            )
-        return np.array(
-            [
-                [1, 0, 0, 0],
-                [0, 1, 0, 0],
-                [0, 0, entry_1, entry_2],
-                [0, 0, entry_3, entry_4],
-            ]
-        )
+        transform_matrix = np.array([[entry_1, entry_2], [entry_3, entry_4]])
 
-    return np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+    if "0)" in gate:
+        return np.kron(iden_matrix, transform_matrix)
+    return np.kron(transform_matrix, iden_matrix)
 
 
-probabilities, vector, conjugate = statevector_output(["gate.h(0)", "gate.h(1)"])
-print(probabilities)
+a, b, c = statevector_output(["state.h(0)", "best.h(1)"])
+print(a[1])
+print(b)
+print(c)
