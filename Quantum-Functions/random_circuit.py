b2195024730e1a7147cd9e71d3e55674e8aebc7c
MihirV17 <mvemuri@olin.edu>
2023-03-27 22:05:55 -0400

    Commiting random circuit

--- Quantum-Functions/random_circuit.py
+++ Quantum-Functions/random_circuit.py
@@ -1,79 +1,183 @@
 """
-A program that randomizes which logic gates
-quibits go into.
+This is a program which initializes a random path of quantum gates, and
+then evaluates each gate in the path within it's specified parameters(Random
+angle values, Random qubit values, etc).
 """
 
-from random import choice, randrange, uniform
+# Import statements(including qiskit methods)
 import numpy as np
-from qiskit import IBMQ, QuantumCircuit, transpile
+from random import choice, randrange
+from qiskit import QuantumCircuit, Aer, execute
 
+# Define the potential gates as a dictionary
 POTENTIAL_GATES = {
-    1: QuantumCircuit.h,
-    2: QuantumCircuit.y,
-    3: QuantumCircuit.x,
-    4: QuantumCircuit.z,
-    5: QuantumCircuit.p,
-    6: QuantumCircuit.s,
-    7: QuantumCircuit.sdg,
-    8: QuantumCircuit.t,
-    9: QuantumCircuit.tdg,
-    10: QuantumCircuit.cx,
-    11: QuantumCircuit.swap,
-    12: QuantumCircuit.sx,
-    13: QuantumCircuit.sxdg,
-    14: QuantumCircuit.rx,
-    15: QuantumCircuit.ry,
-    16: QuantumCircuit.rz,
-    17: QuantumCircuit.rxx,
-    18: QuantumCircuit.ryy,
-    19: QuantumCircuit.rzz,
-    20: QuantumCircuit.u,
+    0: ".u(",
+    1: ".h(",
+    2: ".y(",
+    3: ".x(",
+    4: ".z(",
+    5: ".p(",
+    6: ".s(",
+    7: ".sdg(",
+    8: ".t(",
+    9: ".tdg(",
+    10: ".cx(",
+    11: ".swap(",
+    12: ".sx(",
+    13: ".sxdg(",
+    14: ".rx(",
+    15: ".ry(",
+    16: ".rz(",
+    17: ".rxx(",
+    18: ".ryy(",
+    19: ".rzz(",
 }
 
 
-def random_circuit(depth, gatelist):
+def random_circuit(depth, gatelist=[]):
     """
-    Returns a randomly generated path of quantum gates that a quibit goes into
-    based on previously defined functions from the "QuantumCircuit" file. These quantum gates
-    are listed:
-
-    Hadamard Gate: The H-Gate enables us to travel away from the Bloch sphere's
-    poles and produce a superposition of 0 and 1. It basically transforms the state
-    of a qubit between the x and z bases.
-    X Gate: The x gate is a qubit rotation through pi radians around the x axis
-    Y Gate: The y gate is a qubit rotation through pi radians around the y axis
-    Z Gate: The z gate is a qubit rotation through pi radians around the z axis
-    P Gate: The P-gate (phase gate) requires a number to tell it exactly what to perform
-    because it is parametrized. The P-gate rotates degrees in the direction of the Z-axis.
-    S Gate: The S gate is known as the phase gate or the Z90 gate, because it represents
-    a 90-degree rotation around the z-axis.
-    T Gate: S = T^2
+    Returns a randomized list of quantum gates based on
+    a specified depth paramenter. This list represents
+    a path of gates that a randomized qubit is passed through,
+    which then effectively changes it's superposition. This
+    randomized path can be used for a random circuit vizualization.
 
     Args:
-    depth: An int that represents the amount of a gates a qubit should
-    be passed through.
-
+        depth: an int which specifies the amount of quantum gates that
+        will be included in the final list.
+        gatelist: an empty list(defined in the arguments) that is used in
+        appending each of the randomized gates into it
 
     Returns:
+        A list of strings(each mapped to a key in the global POTENTIAL_GATES
+        dictionary) that represents randomized quantum gates and the path an
+        inputted qubit would take.
 
     """
+
+    # Base Case if the specified depth asks for no quantum fucntions
     if depth == 0:
         return gatelist
 
-    gate = list(POTENTIAL_GATES.keys())
-    random_use = choice([0, 1])
-    theta = np.random.uniform(0, 2 * np.pi)
-    lam = np.random.uniform(0, 2 * np.pi)
-    phi = np.random.uniform(0, 2 * np.pi)
+    # Intialize a variable that gives a random range based on the length of POTENTIAL_GATES
     gate_select = randrange(len(POTENTIAL_GATES))
-    qubit_select = choice([0, 1])
 
+    # Append a random gate to the initalized empty gatelist and recursively return the output
     gatelist.append(POTENTIAL_GATES[gate_select])
     return random_circuit(depth - 1, gatelist)
 
 
-def evaluate_circuit(potential_gates)
+def evaluate_circuit(gatelist):
+    qubit_input_list = []
+    circuit = QuantumCircuit(2)
+    theta = np.random.uniform(0, 2 * np.pi)
+    lam = np.random.uniform(0, 2 * np.pi)
+    phi = np.random.uniform(0, 2 * np.pi)
+    qubit_select = choice([0, 1])
+    for gate in gatelist:
+        if gate == ".x(":
+            circuit.x(qubit_select)
+            qubit_input_list.append(".x(" + str(qubit_select) + ")")
+        elif gate == ".y(":
+            circuit.y(qubit_select)
+            qubit_input_list.append(".y(" + str(qubit_select) + ")")
+        elif gate == ".z(":
+            circuit.z(qubit_select)
+            qubit_input_list.append(".z(" + str(qubit_select) + ")")
+        elif gate == ".h(":
+            circuit.h(qubit_select)
+            qubit_input_list.append(".h(" + str(qubit_select) + ")")
+        elif gate == ".sx(":
+            circuit.sx(qubit_select)
+            qubit_input_list.append(".sx(" + str(qubit_select) + ")")
+        elif gate == ".sxdg(":
+            circuit.sxdg(qubit_select)
+            qubit_input_list.append(".sxdg(" + str(qubit_select) + ")")
+        elif gate == ".t(":
+            circuit.t(qubit_select)
+            qubit_input_list.append(".t(" + str(qubit_select) + ")")
+        elif gate == ".p(":
+            circuit.p(theta, qubit_select)
+            qubit_input_list.append(".p(" + str(theta) + ", " + str(qubit_select) + ")")
+        elif gate == ".tdg(":
+            circuit.tdg(qubit_select)
+            qubit_input_list.append(".t(" + str(qubit_select) + ")")
+        elif gate == ".cx(":
+            if qubit_select == 0:
+                circuit.cx(qubit_select, 1)
+                qubit_input_list.append(".cx(0, 1)")
+            else:
+                circuit.cx(qubit_select, 0)
+                qubit_input_list.append(".cx(1, 0)")
+        elif gate == ".rxx(":
+            if qubit_select == 0:
+                circuit.rxx(theta, qubit_select, 1)
+                qubit_input_list.append(".rxx(" + str(theta) + ", 0, 1)")
+            else:
+                circuit.rxx(theta, qubit_select, 0)
+                qubit_input_list.append(".rxx(" + str(theta) + ", 1, 0)")
+        elif gate == ".ryy(":
+            if qubit_select == 0:
+                circuit.ryy(theta, qubit_select, 1)
+                qubit_input_list.append(".ryy(" + str(theta) + ", 0, 1)")
+            else:
+                circuit.ryy(theta, qubit_select, 0)
+                qubit_input_list.append(".ryy(" + str(theta) + ", 0, 1)")
+        elif gate == ".rzz(":
+            if qubit_select == 0:
+                circuit.rzz(theta, qubit_select, 1)
+                qubit_input_list.append(".rzz(" + str(theta) + ", 0, 1)")
+            else:
+                circuit.rzz(theta, qubit_select, 0)
+                qubit_input_list.append(".rzz(" + str(theta) + ", 0, 1)")
+        elif gate == ".rx(":
+            circuit.rx(theta, qubit_select)
+            qubit_input_list.append(
+                ".rx(" + str(theta) + ", " + str(qubit_select) + ")"
+            )
+        elif gate == ".ry(":
+            circuit.ry(theta, qubit_select)
+            qubit_input_list.append(
+                ".ry(" + str(theta) + ", " + str(qubit_select) + ")"
+            )
+        elif gate == ".rz(":
+            circuit.rz(theta, qubit_select)
+            qubit_input_list.append(
+                ".rz(" + str(theta) + ", " + str(qubit_select) + ")"
+            )
+        elif gate == ".s(":
+            circuit.s(qubit_select)
+            qubit_input_list.append(".s(" + str(qubit_select) + ")")
+        elif gate == ".sdg(":
+            circuit.sdg(qubit_select)
+            qubit_input_list.append(".sdg(" + str(qubit_select) + ")")
+        elif gate == ".swap(":
+            if qubit_select == 0:
+                circuit.swap(qubit_select, 1)
+                qubit_input_list.append(".rz(" + str(qubit_select) + ",1)")
+            else:
+                circuit.swap(qubit_select, 0)
+                qubit_input_list.append(".rz(" + str(qubit_select) + ",0)")
+        elif gate == ".u(":
+            circuit.u(theta, phi, lam, qubit_select)
+            qubit_input_list.append(
+                ".u("
+                + str(theta)
+                + ", "
+                + str(phi)
+                + ", "
+                + str(lam)
+                + ", "
+                + str(qubit_select)
+                + ")"
+            )
+    circuit.measure_all()
+    simulator = Aer.get_backend("qasm_simulator")
+    result = execute(circuit, simulator, shots=1024).result()
+    counts = result.get_counts(circuit)
+    return qubit_input_list
 
 
-eval("circuit.h(0)")
-print(circuit)
+gatelist = random_circuit(3)
+print(evaluate_circuit(gatelist))
+print(gatelist)
